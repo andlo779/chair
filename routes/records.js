821a38f0d9c95882b053175816bdc771cf086c9d
andlo779 <andlo779@gmail.com>
2016-03-13 17:27:26 +0100

    Added all CRUD methods for records route.

--- routes/records.js
+++ routes/records.js
@@ -4,13 +4,44 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var record = require('../models/record.js');
 
+/* POST one record. */
+router.post('/', function(req, res, next) {
+	record.create(req.body, function(err, post) {
+		if (err) return next(err);
+		res.json(post);
+	})
+})
+
 /* GET all records. */
 router.get('/', function(req, res, next) {
   record.find(function(err, records) {
   	if (err) return next(err);
   	res.json(records);
-  }
-  	)
+  })
 });
+
+/* GET one record. */
+router.get('/:id', function(req, res, next) {
+	record.findById(req.params.id, function(err, record) {
+		if (err) return next(err);
+		res.json(record);
+	})
+})
+
+/* PUT one record. */
+router.put('/:id', function(req, res, next) {
+  record.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+		if (err) return next(err);
+		res.json(post);
+	})
+})
+
+/* DELETE one record. */
+router.delete('/:id', function(req, res, next) {
+  record.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+  	if (err) return next(err);
+  	res.json(post);
+  })
+})
 
 module.exports = router;
